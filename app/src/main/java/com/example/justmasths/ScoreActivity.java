c7cbabf54e9254071403e2614fef85ac924cbee5
anilgudur <anilg.cse@gmail.com>
2020-12-17 19:22:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/justmasths/ScoreActivity.java
@@ -0,0 +1,19 @@
+package com.example.justmasths;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+
+        TextView tv = (TextView) findViewById(R.id.score);
+        int score = getIntent().getIntExtra("score", 0);
+        tv.setText("Current Score : " + score);
+    }
+}
